2c4d7c0449b0b45469950620b155481317b43e55
Jhojan Tobón <jhojantobonm@gmail.com>
2023-08-19 15:26:30 -0500

    routing created

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-console.log('My App');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Main');
 
+});
+
+app.get('/categories',(req,res)=>{
+  res.send('Categories');
+});
+
+app.get('/products',(req,res)=>{
+
+  res.json({
+    name: 'computer',
+    price: 1000
+
+  });
 });
 
 app.listen(port,()=>{
