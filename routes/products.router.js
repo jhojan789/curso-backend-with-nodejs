b5fa300bd8edd6fdc7e1aa0fa7fd95ba27381040
Jhojan Tobón <jhojantobonm@gmail.com>
2023-08-21 19:37:47 -0500

    creating services find, findOne

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const router = express.Router();
-const faker = require('faker');
+const ProductService = require('./../services/product.services');
 
-
+const service =  new ProductService();
 
 // IN ORDER THIS products/filter END POINT TO WORK MUST BE GO BEFORE ANY ENDPOINTS WITH DYNAMIC
 // PARAMETERS, IN THIS CASE MUST GO BEFORE products and products/:id.
@@ -12,39 +12,26 @@ router.get('/filter',(req,res)=>{
 });
 
 router.get('/',(req,res)=>{
-  const {size} = req.query;
-  const products = [];
-  const limit = size || 10;
-
-  for (let i = 0; i < limit; i++) {
-
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl(),
-
-    });
-
-  }
-
-  res.json(products);
+  const products = service.find();
+  res.status(200).json(products);
 });
 
 
 router.get('/:id',(req,res)=>{
   const { id } = req.params;
-  res.json(
-    {
-      id,
-      name: 'sofa',
-      price: 2000,
-
-    });
+  const product = service.findOne(id);
+  if(product)
+    res.status(200).json(product);
+  else
+    res.status(404).json({
+      message: '404: NOT FOUND'
+    })
   });
+
 
   router.post('/', (req,res)=>{
     const body = req.body;
-    res.json(
+    res.status(201).json(
       {
         message: "created",
         data : body,
